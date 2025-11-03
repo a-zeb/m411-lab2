b0cc5390d3170702e8c3e06922e2d3a1c1908fb1
azebusmc <65170242+azebusmc@users.noreply.github.com>
2025-11-03 15:26:18 -0500

    Added basic code and reflection questions

---
+++ README.md
@@ -0,0 +1,10 @@
+REFLECTION
+
+How did array methods like push() and filter() help you manipulate data in this activity?
+They helped me to implement the add and search functionality.
+
+What challenges did you face when trying to filter and search for items in the array?
+Getting the syntax correct.
+
+How would you modify this code to make it more efficient or user-friendly?
+I would style it with bootstrap.
--- app.js
+++ app.js
@@ -1,25 +1,45 @@
-shoppingList = []
+shoppingList = [];
 
-function addItem (item) {
-    if (shoppingList.includes(item)) {
-        console.log("Item already exists")
-    } else {
-        shoppingList.push(item)
-    }
+let removeLastItemButton = document.body.getElementById("removeLastItem");
+removeLastItemButton.addEventListener("click", removeLastItem());
+
+let addItemButton = document.body.getElementById("addItemButton");
+addItemButton.addEventListener("click", addItem());
+
+function initializeList() {
+  let shoppingListElement = document.body.getElementById("itemList");
+  shoppingList.forEach((item) => {
+    let newListItemElement = document.body.createElement("li");
+    newListItemElement.textContent = item;
+    shoppingListElement.appendChild(listItem);
+  });
 }
 
-function removeLastItem () {
-    shoppingList.pop()
+function addItem() {
+  let item = getElementById("addItemText").textContent;
+  if (shoppingList.includes(item)) {
+    console.log("Item already exists");
+  } else {
+    shoppingList.push(item);
+    initializeList();
+    // let shoppingListElement = document.getElementById("itemList");
+    // let newListItemElement = document.createElement("li");
+  }
 }
 
-function displayList () {
-    shoppingList.forEach(item => {
-        console.log(item)
-    });
+function removeLastItem() {
+  shoppingList.pop();
 }
 
-addItem(1)
-addItem(2)
-addItem(3)
-removeLastItem()
-displayList()
+function filterItems(searchTerm) {
+  let filtered = shoppingList.filter((item) =>
+    item.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  return filtered;
+}
+
+function displayList() {
+  shoppingList.forEach((item) => {
+    console.log(item);
+  });
+}
